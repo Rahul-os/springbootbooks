cca86c50909cda0473b89889feee8ca2daf99637
Rahul-os <63527466+Rahul-os@users.noreply.github.com>
2023-03-12 21:56:00 +0530

    Add files via upload

---
+++ springbootbooks/src/main/java/books/Sample.java
@@ -0,0 +1,32 @@
+package books;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sample")
+public class Sample {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int code;
+	@Column(length = 20)
+	private String name;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
